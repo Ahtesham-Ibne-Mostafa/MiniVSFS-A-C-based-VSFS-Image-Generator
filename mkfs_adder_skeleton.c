9f541f8079dada1628cf786c0580d5c9316794b2
Ahtesham Ibne Mostafa <ahtesham.ibne.mostafa@g.bracu.ac.bd>
2025-09-06 21:21:03 +0600

    Add files via upload

--- mkfs_adder_skeleton.c
+++ mkfs_adder_skeleton.c
@@ -1,8 +1,12 @@
 #define _FILE_OFFSET_BITS 64
 #include <stdio.h>
 #include <stdint.h>
-
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <libgen.h>
 #define BS 4096u
+
 #define INODE_SIZE 128u
 #define ROOT_INO 1u
 #define DIRECT_MAX 12
@@ -14,6 +18,14 @@ typedef struct {
     
     // THIS FIELD SHOULD STAY AT THE END
     // ALL OTHER FIELDS SHOULD BE ABOVE THIS
+    uint32_t magic, version, block_size;
+    uint64_t total_blocks, inode_count;
+    uint64_t inode_bitmap_start, inode_bitmap_blocks;
+    uint64_t data_bitmap_start, data_bitmap_blocks;
+    uint64_t inode_table_start, inode_table_blocks;
+    uint64_t data_region_start, data_region_blocks;
+    uint64_t root_inode, mtime_epoch;
+    uint32_t flags;
     uint32_t checksum;            // crc32(superblock[0..4091])
 } superblock_t;
 #pragma pack(pop)
@@ -26,6 +38,21 @@ typedef struct {
 
     // THIS FIELD SHOULD STAY AT THE END
     // ALL OTHER FIELDS SHOULD BE ABOVE THIS
+    uint16_t mode;
+    uint16_t links;
+    uint32_t uid;
+    uint32_t gid;
+    uint64_t size_bytes;
+    uint64_t atime;
+    uint64_t mtime;
+    uint64_t ctime;
+    uint32_t direct[12];
+    uint32_t reserved_0;
+    uint32_t reserved_1;
+    uint32_t reserved_2;
+    uint32_t proj_id;
+    uint32_t uid16_gid16;
+    uint64_t xattr_ptr;
     uint64_t inode_crc;   // low 4 bytes store crc32 of bytes [0..119]; high 4 bytes 0
 
 } inode_t;
@@ -36,7 +63,9 @@ _Static_assert(sizeof(inode_t)==INODE_SIZE, "inode size mismatch");
 typedef struct {
     // CREATE YOUR DIRECTORY ENTRY STRUCTURE HERE
     // IF CREATED CORRECTLY, THE STATIC_ASSERT ERROR SHOULD BE GONE
-
+    uint32_t inode_no;
+    uint8_t type;
+    char name[58];
     uint8_t  checksum; // XOR of bytes 0..62
 } dirent64_t;
 #pragma pack(pop)
@@ -86,11 +115,151 @@ void dirent_checksum_finalize(dirent64_t* de) {
     de->checksum = x;
 }
 
-int main() {
+int main(int argc, char* argv[]) {
     crc32_init();
-    // WRITE YOUR DRIVER CODE HERE
-    // PARSE YOUR CLI PARAMETERS
-    // THEN ADD THE SPECIFIED FILE TO YOUR FILE SYSTEM
-    // UPDATE THE .IMG FILE ON DISK
+
+    if (argc != 7) {
+        fprintf(stderr, "Usage: --input <in.img> --output <out.img> --file <filename>\n");
+        return 1;
+    }
+
+    const char *input_img = NULL, *output_img = NULL, *filename = NULL;
+    for (int i = 1; i < argc; i++) {
+        if (!strcmp(argv[i], "--input")) input_img = argv[++i];
+        else if (!strcmp(argv[i], "--output")) output_img = argv[++i];
+        else if (!strcmp(argv[i], "--file")) filename = argv[++i];
+    }
+
+    if (!input_img || !output_img || !filename) {
+        fprintf(stderr, "Missing required arguments.\n");
+        return 1;
+    }
+
+    FILE *fin = fopen(input_img, "rb");
+    if (!fin) {
+        perror("Failed to open input image");
+        return 1;
+    }
+
+    superblock_t sb;
+    fread(&sb, sizeof(sb), 1, fin);
+
+    uint64_t total_bytes = sb.total_blocks * BS;
+    uint8_t *image = malloc(total_bytes);
+    if (!image) {
+        fprintf(stderr, "Memory allocation failed.\n");
+        fclose(fin);
+        return 1;
+    }
+    fseek(fin, 0, SEEK_SET);
+    fread(image, 1, total_bytes, fin);
+    fclose(fin);
+
+    uint8_t *inode_bitmap = image + BS * sb.inode_bitmap_start;
+    uint8_t *data_bitmap = image + BS * sb.data_bitmap_start;
+    inode_t *inode_table = (inode_t *)(image + BS * sb.inode_table_start);
+
+    FILE *fdata = fopen(filename, "rb");
+    if (!fdata) {
+        perror("Failed to open file to add");
+        free(image);
+        return 1;
+    }
+
+    fseek(fdata, 0, SEEK_END);
+    uint64_t fsize = ftell(fdata);
+    fseek(fdata, 0, SEEK_SET);
+
+    uint32_t blocks_needed = (fsize + BS - 1) / BS;
+    if (blocks_needed > DIRECT_MAX) {
+        fprintf(stderr, "File too large for MiniVSFS (max %d blocks).\n", DIRECT_MAX);
+        fclose(fdata);
+        free(image);
+        return 1;
+    }
+
+    int free_ino = -1;
+    for (uint64_t i = 0; i < sb.inode_count; i++) { // Fix signed/unsigned comparison
+        if (!(inode_bitmap[i / 8] & (1 << (i % 8)))) {
+            free_ino = i;
+            inode_bitmap[i / 8] |= (1 << (i % 8));
+            break;
+        }
+    }
+    if (free_ino == -1) {
+        fprintf(stderr, "No free inode available.\n");
+        fclose(fdata);
+        free(image);
+        return 1;
+    }
+
+    uint32_t data_blocks[DIRECT_MAX] = {0};
+    uint64_t found = 0; // Fix signed/unsigned comparison
+    for (uint64_t i = 0; i < sb.data_region_blocks && found < (uint64_t)blocks_needed; i++) {
+        if (!(data_bitmap[i / 8] & (1 << (i % 8)))) {
+            data_bitmap[i / 8] |= (1 << (i % 8));
+            data_blocks[found++] = sb.data_region_start + i;
+        }
+    }
+    if ((uint64_t)found < (uint64_t)blocks_needed) {
+        fprintf(stderr, "Not enough free data blocks.\n");
+        fclose(fdata);
+        free(image);
+        return 1;
+    }
+
+    inode_t *new_inode = &inode_table[free_ino];
+    memset(new_inode, 0, sizeof(inode_t));
+    new_inode->mode = 0100000;
+    new_inode->links = 1;
+    new_inode->uid = 0;
+    new_inode->gid = 0;
+    new_inode->size_bytes = fsize;
+    new_inode->atime = new_inode->mtime = new_inode->ctime = time(NULL);
+    memcpy(new_inode->direct, data_blocks, blocks_needed * sizeof(uint32_t));
+    inode_crc_finalize(new_inode);
+
+    uint64_t remaining = fsize;
+    for (uint32_t i = 0; i < blocks_needed; i++) {
+        uint8_t *block_ptr = image + BS * data_blocks[i];
+        size_t to_read = (remaining > BS) ? BS : remaining;
+        fread(block_ptr, 1, to_read, fdata);
+        remaining -= to_read;
+    }
+    fclose(fdata);
+
+    inode_t *root_inode = &inode_table[ROOT_INO - 1];
+    if (root_inode->direct[0] == 0) {
+        fprintf(stderr, "Root inode has no data block allocated.\n");
+        free(image);
+        return 1;
+    }
+    uint8_t *root_dir_block = image + BS * root_inode->direct[0];
+
+    dirent64_t *entry = (dirent64_t *)(root_dir_block + root_inode->size_bytes);
+    memset(entry, 0, sizeof(dirent64_t));
+    entry->inode_no = free_ino + 1;
+    entry->type = 1;
+    char namebuf[58];
+    strncpy(namebuf, basename((char *)filename), 57);
+    namebuf[57] = '\0';
+    strncpy(entry->name, namebuf, 58);
+    dirent_checksum_finalize(entry);
+
+    root_inode->size_bytes += sizeof(dirent64_t);
+    root_inode->links += 1;
+    inode_crc_finalize(root_inode);
+
+    superblock_crc_finalize((superblock_t *)image);
+
+    FILE *fout = fopen(output_img, "wb");
+    if (!fout) {
+        perror("Failed to open output image");
+        free(image);
+        return 1;
+    }
+    fwrite(image, 1, total_bytes, fout);
+    fclose(fout);
+    free(image);
     return 0;
 }
--- mkfs_builder_skeleton.c
+++ mkfs_builder_skeleton.c
@@ -25,6 +25,22 @@ typedef struct {
     
     // THIS FIELD SHOULD STAY AT THE END
     // ALL OTHER FIELDS SHOULD BE ABOVE THIS
+    uint32_t magic;
+    uint32_t version;
+    uint32_t block_size;
+    uint64_t total_blocks;
+    uint64_t inode_count;
+    uint64_t inode_bitmap_start;
+    uint64_t inode_bitmap_blocks;
+    uint64_t data_bitmap_start;
+    uint64_t data_bitmap_blocks;
+    uint64_t inode_table_start;
+    uint64_t inode_table_blocks;
+    uint64_t data_region_start;
+    uint64_t data_region_blocks;
+    uint64_t root_inode;
+    uint64_t mtime_epoch;
+    uint32_t flags;
     uint32_t checksum;            // crc32(superblock[0..4091])
 } superblock_t;
 #pragma pack(pop)
@@ -37,7 +53,22 @@ typedef struct {
 
     // THIS FIELD SHOULD STAY AT THE END
     // ALL OTHER FIELDS SHOULD BE ABOVE THIS
-    uint64_t inode_crc;   // low 4 bytes store crc32 of bytes [0..119]; high 4 bytes 0
+    uint16_t mode;
+    uint16_t links;
+    uint32_t uid;
+    uint32_t gid;
+    uint64_t size_bytes;
+    uint64_t atime;
+    uint64_t mtime;
+    uint64_t ctime;
+    uint32_t direct[12];
+    uint32_t reserved_0;
+    uint32_t reserved_1;
+    uint32_t reserved_2;
+    uint32_t proj_id;
+    uint32_t uid16_gid16;
+    uint64_t xattr_ptr;
+    uint64_t inode_crc;  // low 4 bytes store crc32 of bytes [0..119]; high 4 bytes 0
 
 } inode_t;
 #pragma pack(pop)
@@ -47,7 +78,9 @@ _Static_assert(sizeof(inode_t)==INODE_SIZE, "inode size mismatch");
 typedef struct {
     // CREATE YOUR DIRECTORY ENTRY STRUCTURE HERE
     // IF CREATED CORRECTLY, THE STATIC_ASSERT ERROR SHOULD BE GONE
-
+    uint32_t inode_no;
+    uint8_t  type;       // 1=file, 2=dir
+    char     name[58];
     uint8_t  checksum; // XOR of bytes 0..62
 } dirent64_t;
 #pragma pack(pop)
@@ -97,11 +130,123 @@ void dirent_checksum_finalize(dirent64_t* de) {
     de->checksum = x;
 }
 
-int main() {
+int main(int argc, char* argv[]) {
+
     crc32_init();
     // WRITE YOUR DRIVER CODE HERE
     // PARSE YOUR CLI PARAMETERS
     // THEN CREATE YOUR FILE SYSTEM WITH A ROOT DIRECTORY
     // THEN SAVE THE DATA INSIDE THE OUTPUT IMAGE
+    const char* image_name = NULL;
+    uint64_t size_kib = 0, inode_count = 0;
+
+    for (int i = 1; i < argc; i++) {
+        if (!strcmp(argv[i], "--image") && i+1 < argc) image_name = argv[++i];
+        else if (!strcmp(argv[i], "--size-kib") && i+1 < argc) size_kib = strtoull(argv[++i], NULL, 10);
+        else if (!strcmp(argv[i], "--inodes") && i+1 < argc) inode_count = strtoull(argv[++i], NULL, 10);
+        else {
+            fprintf(stderr, "Unknown or incomplete flag near '%s'\n", argv[i]);
+            return 2;
+        }
+    }
+
+    if (!image_name || size_kib < 180 || size_kib > 4096 || (size_kib % 4) != 0 ||
+        inode_count < 128 || inode_count > 512) {
+        fprintf(stderr, "Usage: --image <out.img> --size-kib <180..4096, multiple of 4> --inodes <128..512>\n");
+        return 2;
+    }
+
+    uint64_t total_blocks = (size_kib * 1024u) / BS;
+    uint64_t inode_table_blocks = (inode_count * INODE_SIZE + BS - 1) / BS;
+
+    const uint64_t inode_bitmap_start = 1;
+    const uint64_t data_bitmap_start  = 2;
+    const uint64_t inode_table_start  = 3;
+    const uint64_t data_region_start  = inode_table_start + inode_table_blocks;
+    if (data_region_start >= total_blocks) {
+        fprintf(stderr, "Configuration leaves no data region.\n");
+        return 2;
+    }
+    const uint64_t data_region_blocks = total_blocks - data_region_start;
+
+    // Allocate and zero entire image
+    uint8_t* image = (uint8_t*)calloc(total_blocks, BS);
+    if (!image) { perror("calloc"); return 1; }
+
+    // Build and place superblock into block 0
+    time_t now = time(NULL);
+    superblock_t sb = {
+        .magic = 0x4D565346u,        // "MVFS"
+        .version = 1u,
+        .block_size = BS,
+        .total_blocks = total_blocks,
+        .inode_count = inode_count,
+        .inode_bitmap_start = inode_bitmap_start,
+        .inode_bitmap_blocks = 1u,
+        .data_bitmap_start = data_bitmap_start,
+        .data_bitmap_blocks = 1u,
+        .inode_table_start = inode_table_start,
+        .inode_table_blocks = inode_table_blocks,
+        .data_region_start = data_region_start,
+        .data_region_blocks = data_region_blocks,
+        .root_inode = ROOT_INO,
+        .mtime_epoch = (uint64_t)now,
+        .flags = 0u,
+        .checksum = 0u
+    };
+    // Copy struct into block 0; block tail stays zero
+    memcpy(image + 0*BS, &sb, sizeof(sb));
+    // Compute checksum over the entire 4 KiB block (not the stack struct)
+    superblock_crc_finalize((superblock_t*)(image + 0*BS));
+
+    // Mark root inode and its first data block in bitmaps
+    uint8_t* inode_bmp = image + inode_bitmap_start*BS;
+    uint8_t* data_bmp  = image + data_bitmap_start*BS;
+
+    inode_bmp[0] |= 0x01; // set bit 0 (inode #1)
+    data_bmp[0]  |= 0x01; // set bit 0 (first data block)
+
+
+    // Create root inode in inode table (inode index 0 -> ino #1)
+    inode_t root = {0};
+    root.mode = 0040000; // directory
+    root.links = 2;      // . and ..
+    root.uid = 0;
+    root.gid = 0;
+    root.size_bytes = 2u * sizeof(dirent64_t);
+    root.atime = root.mtime = root.ctime = (uint64_t)now;
+    memset(root.direct, 0, sizeof(root.direct));
+    root.direct[0] = (uint32_t)data_region_start;
+    root.proj_id = 0;         // set to your group ID if required
+    root.uid16_gid16 = 0;
+    root.xattr_ptr = 0;
+    inode_crc_finalize(&root);
+
+    memcpy(image + inode_table_start*BS + 0*INODE_SIZE, &root, sizeof(root));
+
+    // Write "." and ".." directory entries into the first data block
+    dirent64_t dot = {0};
+    dot.inode_no = ROOT_INO;
+    dot.type = 2;
+    strcpy(dot.name, ".");
+    dirent_checksum_finalize(&dot);
+
+    dirent64_t dotdot = {0};
+    dotdot.inode_no = ROOT_INO;
+    dotdot.type = 2;
+    strcpy(dotdot.name, "..");
+    dirent_checksum_finalize(&dotdot);
+
+    uint8_t* root_block = image + data_region_start*BS;
+    memcpy(root_block, &dot, sizeof(dot));
+    memcpy(root_block + sizeof(dot), &dotdot, sizeof(dotdot));
+
+    // Persist image
+    FILE* f = fopen(image_name, "wb");
+    if (!f) { perror("fopen"); free(image); return 1; }
+    size_t wrote = fwrite(image, BS, total_blocks, f);
+    if (wrote != total_blocks) { perror("fwrite"); fclose(f); free(image); return 1; }
+    fclose(f);
+    free(image);
     return 0;
 }
